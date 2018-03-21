29d7d5ebf4e4b4634e6aaad519cf37d1a403cda8
Dima Legeza <dmytro.legeza@powerhouse.nl>
2018-03-21 13:22:56 +0100

    project re-structure

--- src/main/java/com/dlegeza/stocks/repo/StockRepository.java
+++ src/main/java/com/dlegeza/stocks/repo/StockRepository.java
@@ -1,6 +1,7 @@
 package com.dlegeza.stocks.repo;
 
 import com.dlegeza.stocks.dto.Stock;
+
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
--- src/main/java/com/dlegeza/stocks/rest/StockController.java
+++ src/main/java/com/dlegeza/stocks/rest/StockController.java
@@ -2,15 +2,25 @@ package com.dlegeza.stocks.rest;
 
 import com.dlegeza.stocks.dto.Stock;
 import com.dlegeza.stocks.service.StockService;
-import io.swagger.annotations.*;
-import lombok.RequiredArgsConstructor;
+
+import javax.validation.Valid;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/stocks")
